7c0ca887b55442f9339ab07bd5dc4a8fba0ceb81
Michael Todd Dwyer <michael.todd.dwyer@gmail.com>
2020-04-20 17:02:39 -0400

    added port number

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var talkRouter = require('./routes/talk/talk');
 var app = express();
 
 // view engine setup
-
+app.set('port', process.env.PORT || 3001)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
